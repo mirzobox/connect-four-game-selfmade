d305fb8a87d8844f02f9f0881412827b6a5c8159
mirzoulugbek <mirzoulugbekkhudoyberdiyev@gmail.com>
2023-11-27 15:18:21 +0500

    Set custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    colors: {
+      black: "#000000",
+      "royal-blue": "#5c2dd5",
+      "electric-indigo": "#7945ff",
+      "spicy-pink": "#fd6687",
+      goldenrod: "#ffce67",
+      white: "#ffffff",
+    },
     fontFamily: {
       grotesk: ["Space Grotesk", "Arial", "sans-serif"],
     },
